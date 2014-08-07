d214d9b6b4b6200e676ebf7e9d843dce57faa99c
rektide <rektide@voodoowarez.com>
2014-08-07 14:46:02 -0400

    Create a pipe-maker util, use in transport/pipe.

--- src/transport/pipe.js
+++ src/transport/pipe.js
@@ -1,40 +1,26 @@
 var events= require('events'),
   util= require('util')
-var when= require('when'),
-  pipeline= require('when/pipeline')
-
-var _emit= events.EventEmitter.prototype.emit
+var pipeline= require('../lib/pipeline2')
 
 /**
-  Pipe is an EventEmitter that also has a 'send' method to transmit.
-  This is a base class, setting up _send/_recv command-chains that
-  higher order classes compose on
+  Emit a message, by it's `messageType` if it has one, else as 'message'.
+*/
+function recv(e){
+	var name = e.messageType || 'message'
+	this.emit(name, e)
+}
+
+/**
+  Pipe is an abstract base class that sets up:
+  1. A send method to send data into a pipe (backed by a _send pipeline)
+  2. A recv pipeline a concrete implementation uses to emit messages
 */
 function Pipe(){
 	Pipe.super_.call(this)
-
-	// messages coming out of the pipe will be emitted
-	// here provide a base _recv handler that forwards messages as local events
-	var self = this
-	function recv(ev){
-		var name = ev.messageType ? ev.messageType : 'message'
-		_emit.call(self, name, ev)
-	}
-	recv.owner= this
-
-	this._send = [] // application sending data
-	this._recv = [recv] // receive event from network
+	makePipeline(this, 'send') // application sending data into pipe
+	makePipeline(this, 'recv', recv) // receive data from pipe
 	return this
 }
 util.inherits(Pipe, events.EventEmitter)
-
-/**
-  Send messages into the pipe
-*/
-function send(data){
-	pipeline(this._send, data)
-}
-send.owner= Pipe
-Pipe.prototype.send= send
 
 module.exports = Pipe
