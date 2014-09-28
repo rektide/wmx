d3941c5c15f4c35f444d3fc92e1f5c0c236acae3
rektide <rektide@voodoowarez.com>
2014-09-28 16:45:32 -0400

    Break out a Headers._isForbidden asserter.

--- src/fetch/header.js
+++ src/fetch/header.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
 [Constructor(optional HeadersInit init),
@@ -13,16 +13,29 @@ interface Headers {
 };
 */
 
-module.exports= Header
-module.exports.forbidden= ["Accept-Charset", "Accept-Encoding", "Access-Control-Request-Headers",
-  "Access-Control-Request-Method", "Connection", "Content-Length", "Cookie", "Cookie2", "Date", 
-  "DNT", "Expect", "Host", "Keep-Alive", "Origin", "Referer", "TE", "Trailer", "Transfer-Encoding",
-  "Upgrade", "User-Agent", "Via"] 
-module.exports.forbiddenPrefix= [/^Proxy-/, /^Sec-/]
+module.exports= Headers
+module.exports.forbidden= ['Accept-Charset', 'Accept-Encoding', 'Access-Control-Request-Headers',
+  'Access-Control-Request-Method', 'Connection', 'Content-Length', 'Cookie', 'Cookie2', 'Date', 
+  'DNT', 'Expect', 'Host', 'Keep-Alive', 'Origin', 'Referer', 'TE', 'Trailer', 'Transfer-Encoding',
+  'Upgrade', 'User-Agent', 'Via'].map(lower)
+module.exports.simpleHeader= ['Accept', 'Accept-Language', 'Content-Language', 'Content-Type'].map(lower)
+module.exports.simpleMimeTypes= ['application/x-www-form-urlencoded', 'multipart/form-data', 'text/plain']
+module.exports.forbiddenPrefix= [/^Proxy-/i, /^Sec-/i]
 module.exports.asserts= false
 
-function Headers(map){
-	if(map.length){
+function lower(str){
+	return str.toLowerCase()
+}
+
+function trim(str){
+	return str.trim()
+}
+
+function Headers(map, guards){
+	/*if(map && map.getAll){
+		// no way to iterate inputted Headers without messy deep cloning, yuck!
+		// structural cloning is last
+	}else*/ if(map && map.length){
 		for(var i= 0; i < map.length; ++i){
 			var el= map[i]
 			if(!el || el.length != 2)
@@ -33,49 +46,80 @@ function Headers(map){
 		for(var i in map)
 			this[i]= map[i]
 	}
+	if(guards){
+		this.guards= guards
+	}
 	return this
 }
 
-Headers.prototype.append= function(key, value){
-	if(this[key]
-		this[key].push(value)
-	else
-		this[key] = [value]
-}
-
-Headers.prototype.delete= function(key){
-	var asserts= !!module.exports.asserts
+Headers.prototype.append= (function append(name, value){
 	if(asserts){
-		if(!(key instanceof String)){
-			throw new TypeError('cannot delete non-String-based header')
-		}
-		if(~module.exports.forbidden.indexOf(key))
+		var forbidden = this._isFobidden(name, value)
+		if(err.message)
+			throw err
+		if(err)
 			return
-		for(var i in module.exports.forbiddenPrefix){
-			var check= module.exports.forbiddenPrefix[i]
-			if(check.test(name))
-				return
-		}
 	}
-	delete this[key]
-}
+	if(this[name])
+		this[name].push(value)
+	else
+		this[name] = [value]
+})
 
-Headers.prototype.get= function(key){
-	var vals= this[key]
+Headers.prototype['delete']= (function _delete(name){
+	if(asserts){
+		var forbidden = this._isFobidden(name)
+		if(err.message)
+			throw err
+		if(err)
+			return
+	}
+	delete this[name]
+})
+
+Headers.prototype.get= (function get(name){
+	var vals= this[name]
 	if(vals)
 		return vals[0]
-}
+})
 
-Headers.prototype.getAll= function(key){
-	return this[key] || []
-}
+Headers.prototype.getAll= (function getAll(name){
+	return this[name] || []
+})
 
-Headers.prototype.has= function(key){
-	return !!this[key]
-}
+Headers.prototype.has= (function has(name){
+	return !!this[name]
+})
 
-Headers.prototype.set= function(key, value){
-	this[key]= value.split(';').map(trim)
-}
+Headers.prototype.set= (function set(name, value){
+	if(asserts){
+		var forbidden = this._isFobidden(name, value)
+		if(err.message)
+			throw err
+		if(err)
+			return
+	}
+	this[name]= value.split(';').map(trim)
+})
 
+Headers.prototype._isForbidden= (function _isForbidden(name, value){
+	if(!(name instanceof String)){
+		return TypeError('non-String-based header')
+	}
+	var lower=  name.toLowerCase()
+	if(!~module.exports.forbidden.indexOf(lower))
+		return true
+	for(var i in module.exports.forbiddenPrefix){
+		var check= module.exports.forbiddenPrefix[i]
+		if(check.test(lower))
+			return true
+	}
+	if(this.guards && this.guards.requestNoCors){
+		if(~module.exports.simpleHeader.indexOf(lower))
+			return true
+		if(lower == 'content-type' && value !== undefined && ~module.exports.simpleMimeTypes.indexOf(value))
+			return true
+	}
+	return false
+})
 
