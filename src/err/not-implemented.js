7ad507ad37fadadc48fc12ae2e02162b8143e6c1
rektide <rektide@voodoowarez.com>
2014-09-28 23:18:06 -0400

    Add NotImplementer error.

---
+++ src/err/not-implemented.js
@@ -0,0 +1,19 @@
+module.exports= NotImplemented
+module.exports.make= makeNotImplemented
+
+function NotImplemented(message, name) {
+	this.name = 'NotImplemented';
+	this.message = message;
+	this.stack = (new Error()).stack;
+	return this
+}
+NotImplemented.prototype = new Error;
+
+NotImplemented.mixin= NotImplemented
+
+function makeNotImplemented(o, slot, message){
+	var name= 'NotImplemented:'+o.name||o'::'+slot : ''
+	return function(){
+		throw new NotImplemented(message||'', name)
+	}
+}
