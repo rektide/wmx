5be0f30e290a250ba076b0c1e278513d3de37795
rektide <rektide@voodoowarez.com>
2014-12-09 03:26:57 -0500

    Flyweight-harness style welcomer test.

--- test/browser/router/welcomer.js
+++ test/browser/router/welcomer.js
@@ -1,51 +1,69 @@
-"use strict";
+"use strict"
 
-var tape= require('blue-tape'),
-  when= require('when')
-var arrayReader= require('wmx/wamp/array-reader'),
-  msgs= require('wmx/wamp/msgs'),
-  cross= require('wmx/wamp/transport/cross-document'),
-  welcomer= require('wmx/wamp/router/welcomer')
+var tape= require("blue-tape"),
+  when= require("when")
+var arrayReader= require("wmx/wamp/array-reader"),
+  msgs= require("wmx/wamp/msgs"),
+  cross= require("wmx/wamp/transport/cross-document"),
+  welcomer= require("wmx/wamp/router/welcomer"),
+  crossHarness= require("../pipe/cross-document")
 
-var realm= 'helloer_test_realm',
-  sessionId= 'helloer_test_session',
-  callee= {
-    special: 'bits',
-    roles: {
-      callee: {}
-  }}
+module.exports= WelcomerHarness
+Object.defineProperty(module.exports, "Harness", {
+	get: WelcomerHarness
+})
 
-tape('Welcomer replies to a raw message', function(t){
-	var channel= new MessageChannel(),
-	  pipe= new cross(channel.port1),
-	  welcomer_= new welcomer({realm:realm}, pipe),
+module.exports.realm= "helloer_test_realm"
+module.exports.callee= {
+  special: "bits",
+  roles: {
+    callee: {}}}
+
+function WelcomerHarness(harness){
+	function details(){
+		return harness.details && harness.details !== details ? harness.details : module.exports.callee
+	}
+
+	harness= harness|| {}
+	if(!harness.details){
+		harness.details= details
+	}
+	harness= crossHarness(harness)
+	var realm= harness.realm|| module.exports.realm
+	if(typeof(realm) === "function"){
+		realm= realm.call(harness)
+	}
+	harness.welcomer= new welcomer({realm:realm}, harness.pipe)
+	return harness
+}
+
+tape("Welcomer replies to a raw message", function(t){
+	var harness= WelcomerHarness(),
 	  sent= when.defer(),
 	  received= when.defer()
 
-	channel.port2.onmessage= function(e){
+	harness.channel.port2.onmessage= function(e){
 		var welcome = arrayReader(e.data)
-		t.ok(welcome instanceof msgs.Welcome, 'welcome reply received')
-		t.ok(welcome.session, 'reply has session')
-		t.ok(welcome.details, 'reply has details')
-		t.ok(welcome.details.roles, 'reply has roles')
-		t.ok(welcome.details.roles.dealer, 'reply is dealer')
-		t.ok(!welcome.hello, 'welcome has no hello')
+		t.ok(welcome instanceof msgs.Welcome, "welcome reply received")
+		t.ok(welcome.session, "reply has session")
+		t.ok(welcome.details.roles, "reply has roles")
+		t.ok(welcome.details.roles.dealer, "reply is dealer")
+		t.noOk(welcome.hello, "welcome has no hello")
 		sent.resolve()
 	}
 	
-	welcomer_.on(msgs.Welcome.messageType, function(welcome){
-		//t.ok(welcome instanceof msgs.Welcome, 'welcome is a welcome')
-		t.ok(welcome.messageType, msgs.Welcome.messageType, 'welcome emits of welcome messageType')
-		t.ok(welcome.session, 'welcome emits session')
-		t.ok(welcome.details, 'welcome emits details')
-		t.ok(welcome.details.roles, 'welcome emits roles')
-		t.ok(welcome.details.roles.dealer, 'welcomer emits a dealer')
-		t.ok(welcome.hello, 'welcomer emits hello')
+	harness.welcomer.on(msgs.Hello.messageType, function(welcome){
+		//t.ok(welcome instanceof msgs.Welcome, "welcome is a welcome")
+		t.ok(welcome.messageType, msgs.Welcome.messageType, "welcome emits of welcome messageType")
+		t.equal(welcome.session,"welcome emits session")
+		t.ok(welcome.details, "welcome emits details")
+		t.ok(welcome.details.roles, "welcome emits roles")
+		t.ok(welcome.details.roles.dealer, "welcomer emits a dealer")
+		t.ok(welcome.hello, "welcomer emits hello")
 		received.resolve()
 	})
 
-	// welcomer & listener is setup, start welcomer's port
-	channel.port1.start()
-	channel.port2.postMessage([msgs.Hello.messageType, realm, callee])
+	// welcomer & listener is setup, start welcomer"s port
+	harness.postHello()
 	return when.join(sent.promise, received.promise)
 })
