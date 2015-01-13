bf702809a4d1a94e7c075d3853ecc7e50d707f1c
rektide <rektide@voodoowarez.com>
2015-01-12 21:24:07 -0500

    Let http-server test start, fail. HttpServer mixin EventEmitter.

--- http-server/http-server.js
+++ http-server/http-server.js
@@ -1,4 +1,22 @@
+var emitterMixin= require("emitter-mixin")
+
 function HttpServer(handler){
+	if(!this.listen){ // this test could be better oh-kay?
+		return new HttpServer(handler)
+	}
+	if(!this.on) {
+		emitterMixin(this)
+	}
+	if(this.handler && handler)
+		throw new Exception("You\"ve got a handler sir/madame/")
+	this.handler= handler
+}
+
+HttpServer.prototype.listen= function(pipe){
+	// ensure pipe is encoding yada-yada something-or-other
+	pipe.on(msgs.Call.messageType, function(){
+		console.log("we got a request")
+	})
 }
 
 module.exports= HttpServer
--- test/browser/server/server-1.js
+++ test/browser/server/server-1.js
@@ -1,27 +1,29 @@
 var tape= require("blue-tape")
-var http= require("wmx/http-server"),
-  Welcomer= require("../router/welcomer");
+var http= require('wmx/http-server'),
+  Welcomer= require('../router/welcomer'),
+  CrossDoc= require("wmx/test/browser/pipe/cross-document")
 
-tape("Server", function(t){
-	t.end()
-	return
-	// WIP:
+tape('Server', function(t){
+	//t.end()
+	//return
+	// ^-- WIP-flags
+
 	t.plan(6)
 	function ref(){
 		t.ok()
 	}
 
 	var server= http.createServer(ref)
-	server.on("request", ref)
-	server.on("request", function(req, res){
-		t.equal(req.mesageType, msgs.Call.messageType, "messageType is Call")
-		t.equal(req.method, "GET", "method is GET")
-		t.equal(req.url, "/ping")
-		req.on("data", function(data){
-			t.equal(data, "ping!")
-			res.end("pong")
+	server.on('request', ref)
+	server.on('request', function(req, res){
+		t.equal(req.mesageType, msgs.Call.messageType, 'messageType is Call')
+		t.equal(req.method, 'GET', 'method is GET')
+		t.equal(req.url, '/ping')
+		req.on('data', function(data){
+			t.equal(data, 'ping!')
+			res.end('pong')
 		})
-		req.setEncoding("utf8")
+		req.setEncoding('utf8')
 	});
 
 	
@@ -29,7 +31,7 @@ tape("Server", function(t){
 	server.listen(welcomerPair.welcomer)
 
 	var request= Request({
-		url: "//ping"
+		url: '//ping'
 	})
 	welcomerPair.port2.postMessage(request)
 })
